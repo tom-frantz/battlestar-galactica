a4fe1f6641d9860db53a7d5163924c519b842dfd
BuildTools <unconfigured@null.spigotmc.org>
2016-10-09 11:28:12 +1100

    Deleted Canvas link, changed map link to render 'map_clone'.html, and moved world gen

--- bsg_main.py
+++ bsg_main.py
@@ -1,38 +1,9 @@
 from flask import *
-import random
+import world_gen
 
 app = Flask(__name__)
 
-
-class Galaxy(object):
-	def __init__(self):
-		self.galaxy_x_axis = [-100, 100]
-		self.galaxy_y_axis = [-100, 100]
-		self.planet_abundance = 'Placeholder'
-		self.cylon_intensity = 'placeholder'
-		self.system_list = {0: (-0, -0)}
-
-	def galaxy_generation(self):
-		system_generated = 1
-		for x in range(-100, 100):
-			for y in range(-100, 100):
-				z = random.randint(1, 15)
-				if z == 5:
-					planet_too_close = False
-					for planet in self.system_list:
-						if -1 <= x - self.system_list[planet][0] <= 1:
-							if -1 <= y - self.system_list[planet][1] <= 1:
-								planet_too_close = True
-					if not planet_too_close:
-						self.system_list[system_generated] = [x, y]
-						# print(self.system_list[system_generated])
-						system_generated += 1
-
-	def galaxy_segment_generation(self):
-		# execute this when there is no more galaxy, or closing in on no more galaxy left
-		pass
-
-player_galaxy = Galaxy()
+player_galaxy = world_gen.Galaxy()
 player_galaxy.galaxy_generation()
 
 
@@ -41,22 +12,16 @@ def index():
 	if request.method == 'POST':
 		if request.form['submit'] == 'next_turn':
 			pass
-	return render_template('index.html', player_galaxy=player_galaxy)
+	return render_template('index.html')
 
 
 @app.route('/map', methods=['GET', 'POST'])
-def map():
+def star_map():
+	# Need algorithm for making global to local. Return values from 0 to 60
+	player_galaxy.galaxy_generation()
 	if request.method == 'POST':
 		pass
-	return render_template('map.html', player_galaxy=player_galaxy)
-
-
-@app.route('/canvas', methods=['GET', 'POST'])
-def canvas():
-	if request.method == 'POST':
-		pass
-	return render_template('canvas.html', player_galaxy=player_galaxy)
-
+	return render_template('map_clone.html', player_galaxy=player_galaxy, trim_blocks=True, lstrip_blocks=True)
 
 if __name__ == '__main__':
 	app.run(debug=True)
