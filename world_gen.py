23f6544da4da79254b5f37a6f0459e032b2d5cd4
BuildTools <unconfigured@null.spigotmc.org>
2016-10-09 20:55:27 +1100

    Creating framework for galaxy chunk loading

--- world_gen.py
+++ world_gen.py
@@ -6,9 +6,12 @@ SYSTEM_NAMES_SUFFIXES = ('Prime', 'Alpha', 'Beta', 'Delta', 'Gamma', 'Minor', ''
 
 
 class SolarSystem(object):
-	def __init__(self, position, name):
+	def __init__(self, position, name, total_planets=False):
 		self.global_position = position
-		self.total_planets = random.randint(0, 8)
+		if not total_planets and total_planets != 0:
+			self.total_planets = random.randint(0, 8)
+		else:
+			self.total_planets = total_planets
 		self.planets = {}
 		self.name = name
 
@@ -19,16 +22,20 @@ class SolarSystem(object):
 
 class Galaxy(object):
 	def __init__(self):
+		self.galaxy_x_bounds = (-30, 30)
+		self.galaxy_y_bounds = (-30, 30)
 		self.planet_abundance = 'Placeholder'
 		self.cylon_intensity = 'placeholder'
 		self.system_list = {}
 		self.current_position = (0, 0)
 
 	def galaxy_generation(self):
-		self.system_list['Helios Alpha'] = SolarSystem((0, 0), "Helios Alpha")
+		total_count = 1
+		self.system_list = {}
+		self.system_list['Helios Alpha'] = SolarSystem((0, 0), "Helios Alpha", 4)
 		for x in range(-30, 30):
 			for y in range(-30, 30):
-				system_chance = random.randint(1, 15)
+				system_chance = random.randint(1, 10)
 				if system_chance == 1:
 					system_too_close = False
 					for system in self.system_list:
@@ -46,7 +53,15 @@ class Galaxy(object):
 							system_name = random.choice(SYSTEM_NAMES)
 						self.system_list[system_name] = SolarSystem((x, y), system_name)
 						print(self.system_list[system_name].name, self.system_list[system_name].global_position)
+						total_count += 1
+		print('Total Count:', total_count)
 
 	def galaxy_segment_generation(self):
-		# execute this when there is no more galaxy, or closing in on no more galaxy left
-		pass
+		# top bottom first
+		if self.current_position[1] / 30 > 0.5:
+			pass
+		elif self.current_position[1] / 30 < 0.5:
+			pass
+
+		if True:
+			pass
