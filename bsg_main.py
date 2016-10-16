e112125ff5bbf34bfb934ca73a04920928b429df
BuildTools <unconfigured@null.spigotmc.org>
2016-10-16 21:21:46 +1100

    Work with jQuery and results through Flask pushing and data receivement.

--- bsg_main.py
+++ bsg_main.py
@@ -11,25 +11,20 @@ def next_turn():
 	player_galaxy.galaxy_segment_generation()
 
 
-def galaxy_to_dict(Galaxy):
-	galaxy_dict = {}
-	return galaxy_dict
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
 		if request.form['submit'] == 'next_turn':
 			# Execute the next turn.
 			pass
-	system_list = {}
+	# system_list = {}
 	# for system in player_galaxy.system_list:
 	# 	if player_galaxy.current_position[0] - 30 <= player_galaxy.system_list[system].global_position[0] <= player_galaxy.current_position[0] + 30 \
 	# 	and player_galaxy.current_position[1] - 30 <= player_galaxy.system_list[system].global_position[1] <= player_galaxy.current_position[0] + 30:
 	# 		system_list[system] = player_galaxy.system_list[system]
 
 	dict_player_galaxy = player_galaxy.dictionary_ify()
-	return render_template('template.html', system_list=system_list, player_galaxy=dict_player_galaxy, trim_blocks=True, lstrip_blocks=True)
+	return render_template('template.html', player_galaxy=dict_player_galaxy, trim_blocks=True, lstrip_blocks=True)
 
 
 @app.route('/map', methods=['GET', 'POST'])
@@ -45,5 +40,11 @@ def star_map():
 			system_list[system] = player_galaxy.system_list[system]
 	return render_template('map_clone.html', system_list=system_list, current_position=player_galaxy.current_position, trim_blocks=True, lstrip_blocks=True)
 
+
+@app.route('/trial')
+def trial():
+	dict_player_galaxy = player_galaxy.dictionary_ify()
+	return render_template('trial.html', player_galaxy=dict_player_galaxy, trim_blocks=True, lstrip_blocks=True)
+
 if __name__ == '__main__':
-	app.run(debug=False)
+	app.run(debug=True)
