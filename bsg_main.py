803953f809afbcc28fd5ba197a6bdbc42f039498
Tomato150 <tomcat.frantz@gmail.com>
2016-10-17 15:15:42 +1100

    Cleaned up file

--- bsg_main.py
+++ bsg_main.py
@@ -17,28 +17,8 @@ def index():
 		if request.form['submit'] == 'next_turn':
 			# Execute the next turn.
 			pass
-	# system_list = {}
-	# for system in player_galaxy.system_list:
-	# 	if player_galaxy.current_position[0] - 30 <= player_galaxy.system_list[system].global_position[0] <= player_galaxy.current_position[0] + 30 \
-	# 	and player_galaxy.current_position[1] - 30 <= player_galaxy.system_list[system].global_position[1] <= player_galaxy.current_position[0] + 30:
-	# 		system_list[system] = player_galaxy.system_list[system]
-
 	dict_player_galaxy = player_galaxy.dictionary_ify()
 	return render_template('template.html', player_galaxy=dict_player_galaxy, trim_blocks=True, lstrip_blocks=True)
-
-
-@app.route('/map', methods=['GET', 'POST'])
-def star_map():
-	if request.method == 'POST':
-		if request.form['submit'] == 'next_turn':
-			# Execute next turn
-			pass
-	system_list = {}
-	for system in player_galaxy.system_list:
-		if player_galaxy.current_position[0] - 30 <= player_galaxy.system_list[system].global_position[0] <= player_galaxy.current_position[0] + 30 \
-		and player_galaxy.current_position[1] - 30 <= player_galaxy.system_list[system].global_position[1] <= player_galaxy.current_position[0] + 30:
-			system_list[system] = player_galaxy.system_list[system]
-	return render_template('map_clone.html', system_list=system_list, current_position=player_galaxy.current_position, trim_blocks=True, lstrip_blocks=True)
 
 
 @app.route('/trial')
@@ -47,4 +27,4 @@ def trial():
 	return render_template('trial.html', player_galaxy=dict_player_galaxy, trim_blocks=True, lstrip_blocks=True)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=False)
