f8701b5002803a21f7060649a8a81ff44f5f453f
BuildTools <unconfigured@null.spigotmc.org>
2016-10-19 21:23:25 +1100

    Begun work on classes in JS

---
+++ static/js/bsg_main.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Tom on 19-Oct-16.
+ */
+
+var world;
+world = {
+    system_list: 'dank',
+
+    __init: function myFunction () {
+        console.log('dank');
+        make = 10
+    },
+
+    dank_vars: 'dank memes',
+
+    return: {
+        __init: init
+
+    }
+};
