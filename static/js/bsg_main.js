fc4c467c8cfd0c412202533c501e7ecb43124e13
Tomato150 <tomcat.frantz@gmail.com>
2016-10-20 11:32:33 +1100

    Added junk

--- static/js/bsg_main.js
+++ static/js/bsg_main.js
@@ -2,19 +2,18 @@
  * Created by Tom on 19-Oct-16.
  */
 
-var world;
-world = {
-    system_list: 'dank',
+var world = (function(){
+    var starmap = $('#star-map');
 
-    __init: function myFunction () {
-        console.log('dank');
-        make = 10
-    },
-
-    dank_vars: 'dank memes',
-
-    return: {
-        __init: init
+    function __init() {
 
     }
-};
+
+    function nav_links() {
+
+    }
+
+    return {
+        init: __init
+    }
+});
