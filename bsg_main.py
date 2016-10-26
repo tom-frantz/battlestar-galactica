c77512971c4ae38876de683d8c9376d367a6bcef
BuildTools <unconfigured@null.spigotmc.org>
2016-10-26 21:13:14 +1100

    added menu route.

--- bsg_main.py
+++ bsg_main.py
@@ -1,3 +1,5 @@
+# FOR NOTE: request.form[name] = value
+
 from flask import *
 import galaxy_gen as galaxy
 import navigation as nav
@@ -9,6 +11,19 @@ app = Flask(__name__)
 # Create the galaxy, need to assign to it to a world class eventually with other ai and shit.
 player_galaxy = galaxy.Galaxy()
 player_galaxy.initial_galaxy_generation()
+
+
+@app.route('/menu', methods=['GET', 'POST'])
+def menu():
+	if request.method == 'POST':
+		if request.form.get('dummy', None) == 'default':
+			print('Test Successful')
+			return redirect('/')
+		if request.form.get('random_solar_system', False):
+			print('Random Solar systems = true')
+		else:
+			print('Random Solar System = False')
+	return render_template('index.html', trim_blocks=True, lstrip_blocks=True)
 
 
 # The code that happens for a page load for the base page
