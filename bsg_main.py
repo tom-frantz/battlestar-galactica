8ca753ff994aaf27bbb4571a25b67b64ef054a25
Tomato150 <tomcat.frantz@gmail.com>
2016-10-27 10:09:29 +1100

    Included World class.

--- bsg_main.py
+++ bsg_main.py
@@ -1,41 +1,39 @@
 # FOR NOTE: request.form[name] = value
 
 from flask import *
+import world as world
 import galaxy_gen as galaxy
 import navigation as nav
 import math
 
 # The Flask initialization.
 app = Flask(__name__)
-
-# Create the galaxy, need to assign to it to a world class eventually with other ai and shit.
-player_galaxy = galaxy.Galaxy()
-player_galaxy.initial_galaxy_generation()
+player_world = world.World(galaxy.Galaxy())
 
 
 @app.route('/menu', methods=['GET', 'POST'])
 def menu():
 	if request.method == 'POST':
-		if request.form.get('dummy', None) == 'default':
-			print('Test Successful')
-			return redirect('/')
-		if request.form.get('random_solar_system', False):
-			print('Random Solar systems = true')
-		else:
-			print('Random Solar System = False')
+		if request.form.get('start_game', None) == 'default':
+			player_world.galaxy.initial_galaxy_generation()
+		elif request.form.get('start_game', None) == 'random':
+			player_world.galaxy.initial_galaxy_generation(False)
+		return redirect('/')
 	return render_template('index.html', trim_blocks=True, lstrip_blocks=True)
 
 
 # The code that happens for a page load for the base page
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('template.html', player_galaxy=player_galaxy.dictionary_ify(), trim_blocks=True, lstrip_blocks=True)
+	if not player_world.galaxy.initiated:
+		return redirect('/menu')
+	return render_template('template.html', player_galaxy=player_world.galaxy.dictionary_ify(), trim_blocks=True, lstrip_blocks=True)
 
 
 # A dummy page for any element we're trialing.
 @app.route('/trial')
 def trial():
-	return render_template('trial.html', player_galaxy=player_galaxy.dictionary_ify(), trim_blocks=True, lstrip_blocks=True)
+	return render_template('trial.html', player_galaxy=player_world.galaxy.dictionary_ify(), trim_blocks=True, lstrip_blocks=True)
 
 
 # Code for a AJAX call for the next turn functions.
@@ -44,11 +42,12 @@ def next_turn():
 	jquery_data = request.get_json()
 
 	if jquery_data['next_turn_type'] == 'warp':
-		player_galaxy.current_position = jquery_data['selected_position']
-		current_chunk = [math.floor((player_galaxy.current_position[0] + 30) / 60), math.floor((player_galaxy.current_position[1] + 30) / 60)]
-		if current_chunk != player_galaxy.current_chunk:
-			player_galaxy.current_chunk = current_chunk
-			player_galaxy.galaxy_chunk_generation()
+		# Generate new chunks
+		player_world.galaxy.current_position = jquery_data['selected_position']
+		current_chunk = [math.floor((player_world.galaxy.current_position[0] + 30) / 60), math.floor((player_world.galaxy.current_position[1] + 30) / 60)]
+		if current_chunk != player_world.galaxy.current_chunk:
+			player_world.galaxy.current_chunk = current_chunk
+			player_world.galaxy.galaxy_chunk_generation()
 
 	return jsonify(refresh=True)
 
