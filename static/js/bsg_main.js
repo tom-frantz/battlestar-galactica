01f51c6e1edd308fd03cf11cdc67262685651883
Tomato150 <tomcat.frantz@gmail.com>
2016-10-27 14:41:04 +1100

    began implementation of JavaScript objects.

--- static/js/bsg_main.js
+++ static/js/bsg_main.js
@@ -2,18 +2,25 @@
  * Created by Tom on 19-Oct-16.
  */
 
-var world = (function(){
-    var starmap = $('#star-map');
+var world = ( function() {
 
-    function __init() {
-
+    function __init(World) {
+        var galaxy = World['galaxy'];
     }
 
-    function nav_links() {
+    function canvas_fill(canvas_id, tile_id) {
+        var c = document.getElementById(canvas_id);
+        var ctx = c.getContext("2d");
+        var img = document.getElementById(tile_id);
+        var pat = ctx.createPattern(img, "repeat");
 
+        ctx.rect(0, 0, c.width, c.height);
+        ctx.fillStyle = pat;
+        ctx.fill();
     }
 
     return {
-        init: __init
-    }
-});
+        init: __init,
+        canvas_fill: canvas_fill
+    };
+})();
