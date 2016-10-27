98a323088f75ca1739a39acc7083bd0ded5305c2
Tomato150 <tomcat.frantz@gmail.com>
2016-10-27 14:40:17 +1100

    Fixed serialization and calls

--- world.py
+++ world.py
@@ -1,3 +1,10 @@
 class World(object):
 	def __init__(self, galaxy):
 		self.galaxy = galaxy
+
+	def world_serialize(self):
+		world_dict = {
+			'galaxy': self.galaxy.galaxy_serialize()
+		}
+
+		return world_dict
