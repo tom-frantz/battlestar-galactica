14fdb5e31596ae31c47e5a48889d56388741c925
BuildTools <unconfigured@null.spigotmc.org>
2016-11-06 14:51:35 +1100

    Merged the '/menu' and '/' links

--- bsg_main.py
+++ bsg_main.py
@@ -1,38 +1,47 @@
 # FOR NOTE: request.form[name] = value
 
 from flask import *
+import random, json
+
 import world
-import modules.galaxy_gen as galaxy
 from modules import events, fleets
+import modules.galaxy_gen as galaxy
+
+
+def to_json(obj):
+	return obj.__dict__
 
 # The Flask initialization.
 app = Flask(__name__)
 player_world = world.World(galaxy.Galaxy(), events.EventHandler(), fleets.FleetHandler())
 
 
-@app.route('/menu', methods=['GET', 'POST'])
-def menu():
-	if request.method == 'POST':
-		if request.form.get('start_game', None) == 'default':
-			player_world.initial_galaxy_generation()
-		elif request.form.get('start_game', None) == 'random':
-			player_world.initial_galaxy_generation(False)
-		return redirect('/')
-	return render_template('index.html', trim_blocks=True, lstrip_blocks=True)
-
-
-# The code that happens for a page load for the base page
+# The code that happens for a page load for the base page. Contains the Main Menu HTML render as well as the core game.
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	if not player_world.galaxy.initiated:
-		return redirect('/menu')
-	return render_template('template.html', player_world=player_world.world_serialize(), trim_blocks=True, lstrip_blocks=True)
+	if not player_world.initiated:
+		if request.method == 'POST':
+			seed = request.form.get('seed', None)
+			if request.form.get('seed', None) == '':
+				seed = False
+
+			if request.form.get('start_game', None) == 'default':
+				player_world.initial_galaxy_generation(seed)
+			elif request.form.get('start_game', None) == 'random':
+				player_world.initial_galaxy_generation(seed, False)
+
+		else:
+			return render_template('index.html', trim_blocks=True, lstrip_blocks=True)
+
+	if request.method == 'POST':
+		pass
+	return render_template('main.html', player_world=json.dumps(player_world, default=to_json), trim_blocks=True, lstrip_blocks=True)
 
 
 # A dummy page for any element we're trialing.
 @app.route('/trial')
 def trial():
-	return render_template('trial.html', player_world=player_world.world_serialize(), event=player_world.event_handler.events[0].event_html_serialize(), trim_blocks=True, lstrip_blocks=True)
+	return render_template('trial.html', player_world=json.dumps(player_world, default=to_json), event=player_world.event_handler.events[0].event_html_serialize(), trim_blocks=True, lstrip_blocks=True)
 
 
 # Code for a AJAX call for the next turn functions.
